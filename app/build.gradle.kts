ca3cc7d2c7b751da31396a71640c9ba1af0a6ad7
Samuel-281 <530249138@qq.com>
2026-06-04 00:55:41 +0800

    Bump version to 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.warehouse.pda"
     minSdk = 26
     targetSdk = 35
-    versionCode = 1
-    versionName = "0.1.0"
+    versionCode = 2
+    versionName = "0.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     buildConfigField("String", "DEFAULT_SERVER_URL", "\"http://192.168.1.100:3000/\"")
