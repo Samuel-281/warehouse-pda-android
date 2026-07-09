785f8c88551ff74a9055c2b88e52a8cfad3447e0
Samuel-281 <530249138@qq.com>
2026-07-09 11:11:47 +0800

    chore: bump PDA version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "com.warehouse.pda"
     minSdk = 26
     targetSdk = 35
-    versionCode = 5
-    versionName = "0.1.4"
+    versionCode = 6
+    versionName = "0.1.5"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     buildConfigField("String", "DEFAULT_SERVER_URL", "\"http://192.168.1.100:3000/\"")
